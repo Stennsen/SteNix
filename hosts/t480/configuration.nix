0eceadee175e1c9fbff0d280df843aaceadb719c
Stennsen <dennis@stennsen.dev>
2024-07-21 21:18:38 +0200

    activate ssh agent

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -74,6 +74,7 @@
     packages = with pkgs; [];
   };
 
+  programs.ssh.startAgent = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   # environment.systemPackages = with pkgs; [
